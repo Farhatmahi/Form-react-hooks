f8762bf2e7d19d982b8fdc3b664f0f0cd44f7167
Farhat Mahi <farhatmahi007@gmail.com>
2022-06-16 00:55:16 +0600

    100 lines in 2 lines

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,45 +1,79 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [firstName, setFirstName] = useState("");
-  const [putFirstName, setPutFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [putLastName, setPutLastName] = useState("");
+  const [info, setinfo] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
 
   const inputChange = (event) => {
     console.log(event.target.value);
-    setFirstName(event.target.value);
-  };
+    console.log(event.target.name);
+    const { name, value } = event.target;
 
-  const inputChange2 = (event) => {
-    console.log(event.target.value);
-    setLastName(event.target.value);
+    setinfo((prevValue) => {
+      // console.log(prevValue);
+      // if (name === "fName") {
+      //   return {
+      //     fName: value,
+      //     lName: prevValue.lName,
+      //     email: prevValue.email,
+      //   };
+      // } else if (name === "lName") {
+      //   return {
+      //     fName: prevValue.fName,
+      //     lName: value,
+      //     email: prevValue.email,
+      //   };
+      // } else {
+      //   return {
+      //     fName: prevValue.fName,
+      //     lName: prevValue.lName,
+      //     email: value,
+      //   };
+      // }
+
+      return {
+        ...prevValue,
+        [name] : value,
+      };
+    });
   };
 
   const onSubmitbtn = (event) => {
     event.preventDefault();
-    setPutFirstName(firstName);
-    setPutLastName(lastName);
   };
 
   return (
     <>
       <form onSubmit={onSubmitbtn}>
         <h1>
-          Welcome {putFirstName} {putLastName}
+          Welcome {info.fName} {info.lName}
         </h1>
+        <p>{info.email}</p>
         <input
           type="text"
           placeholder="Enter your first name"
           onChange={inputChange}
-          value={firstName}
+          value={info.fName}
+          name="fName"
         />
         <br />
         <input
           type="text"
           placeholder="Enter your last name"
-          onChange={inputChange2}
-          value={lastName}
+          onChange={inputChange}
+          value={info.lName}
+          name="lName"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter your email"
+          onChange={inputChange}
+          value={info.email}
+          name="email"
         />
         <br />
         <button type="submit">Click here</button>
